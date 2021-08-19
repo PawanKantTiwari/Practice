c0a254a602888ab83dfe57c529d6500880741245
Pawan Kant Tiwari <p.k.t.7042@gmail.com>
2021-08-19 17:04:06 +0530

    maxResult changed from 100 to 5 in youtubeapi.js file

--- youtube_lite/src/apis/youtubeapi.js
+++ youtube_lite/src/apis/youtubeapi.js
@@ -6,7 +6,7 @@ export default axios.create({
     baseURL : "https://www.googleapis.com/youtube/v3/",
     params  : {
         part: "snippet",
-        maxResult : 100,
+        maxResult : 5,
         key : KEY,
     },
 });
