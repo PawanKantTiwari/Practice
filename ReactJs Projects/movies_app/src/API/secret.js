821135ac8232f8386abde8fc774f2fd1fe400f80
Pawan Kant Tiwari <p.k.t.7042@gmail.com>
2021-09-25 20:54:24 +0530

    API Added

---
+++ ReactJs Projects/movies_app/src/API/secret.js
@@ -0,0 +1,8 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = "daefef9a7cd208d6f25a109e48410a8a";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+
+// https://image.tmdb.org/t/p/w500/
+// https://image.tmdb.org/t/p/w500/
+
+// example = "https://api.themoviedb.org/3/movie/550?api_key=daefef9a7cd208d6f25a109e48410a8a"
